4316e4eb8a0908a4312090802284df2bf58df709
suchandra <suchandra.2005@gmail.com>
2016-07-21 22:13:52 +1000

    Initial commit

---
+++ app-services/authentication.service.js
@@ -0,0 +1,52 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('AuthenticationService', AuthenticationService);
+
+    AuthenticationService.$inject = ['$http', '$cookieStore', '$rootScope', '$timeout', '$location'];
+    function AuthenticationService($http, $cookieStore, $rootScope, $timeout, $location) {
+        var service = {};
+
+        service.Login = Login;
+        service.SetCredentials = SetCredentials;
+        service.ClearCredentials = ClearCredentials;
+
+        return service;
+
+        function Login(username, password, callback) {
+            
+            $http({
+			  method: 'POST',
+			  url: '/api/authenticate'
+			}).then(function successCallback(response) {
+			    	callback(response);
+			  }, function errorCallback(response) {
+			    	response = { success: false, message: 'Username or password is incorrect' };
+			    	$location.path('/register');
+			  });
+		}
+
+        function SetCredentials(username, password) {
+            var authdata = Base64.encode(username + ':' + password);
+
+            $rootScope.globals = {
+                currentUser: {
+                    username: username,
+                    authdata: authdata
+                }
+            };
+
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata;
+            $cookieStore.put('globals', $rootScope.globals);
+        }
+
+        function ClearCredentials() {
+            $rootScope.globals = {};
+            $cookieStore.remove('globals');
+            $http.defaults.headers.common.Authorization = 'Basic';
+        }
+    }
+
+})();
