2b9732397b2e9e2c0529dd44699fa264c99b832e
suchandra <suchandra.2005@gmail.com>
2016-10-13 22:49:40 +1000

    subscribe to event implemented

--- eventsCalendar/upcomingEventsCalendar.controller.js
+++ eventsCalendar/upcomingEventsCalendar.controller.js
@@ -8,6 +8,8 @@
     UpcomingEventsCalendarController.$inject = ['$rootScope', 'EventsService', '$scope', 'FlashService'];
     function UpcomingEventsCalendarController($rootScope, EventsService, $scope, FlashService) {
         var vm = this;
+        
+        vm.SubscribeToEvent = SubscribeToEvent;
 
         (function initController() {
             GetAllEvents();
@@ -32,12 +34,36 @@
 					} else {
 						$rootScope.for_next_call = response.data.for_next_call;
 						$rootScope.uID = response.data.uID;
-						$scope.scheduledEvents = (response.data.month_events_list[3]);
 						(response.data.month_events_list).pop();
 						$scope.upcomingEvents = response.data.month_events_list;
-						
+						$scope.showUpcomingEvents = $scope.upcomingEvents[1].length > 0;
 					}                    
                 });
+        }
+        
+        function SubscribeToEvent(eventDetails, event) {
+        	event.stopPropagation();
+        	console.log(eventDetails);
+        	var eventTosubscribe = {
+        		from_prev_call : $rootScope.for_next_call,
+        		uID : $rootScope.uID,
+        		event_row : {
+        			eSEQ : eventDetails[0],
+	        		eNum : eventDetails[1],
+	        		eName : eventDetails[2],
+	        		eLocation : eventDetails[3],
+	        		eStart_date_time : eventDetails[4],
+	        		eEnd_date_time : eventDetails[5],
+	        		oType : eventDetails[6],
+	        		oSkill : eventDetails[7] 
+	        	}
+        	};
+        	console.log(eventTosubscribe);
+        	EventsService.SubscribeToEvent(eventTosubscribe).then(function (response) {
+        		$rootScope.for_next_call = response.data.for_next_call;
+				$rootScope.uID = response.data.uID;
+        		FlashService.Success(response.data.errorMSG_user);
+        	});
         }
         
         $('.tab-list').click(function (e) {
