b76cf34d22caf68ec767d1ac760366eb7c1c7166
suchandra <suchandra.2005@gmail.com>
2016-11-03 19:09:21 +1000

    code cleanp

--- app/app.js
+++ app/app.js
@@ -7,39 +7,39 @@
 	function config($routeProvider, $locationProvider, $httpProvider) {
 		$routeProvider.when('/', {
 			controller : 'HomeController',
-			templateUrl : 'home/home.view.html',
+			templateUrl : 'app/home/home.view.html',
 			controllerAs : 'vm'
 		}).when('/login', {
 			controller : 'LoginController',
-			templateUrl : 'login/login.view.html',
+			templateUrl : 'app/login/login.view.html',
 			controllerAs : 'vm'
 		}).when('/logout', {
 			controller : 'LogoutController',
-			templateUrl : 'login/login.view.html',
+			templateUrl : 'app/login/logout.view.html',
 			controllerAs : 'vm'
 		}).when('/forgotPassword', {
 			controller : 'LoginController',
-			templateUrl : 'login/forgotPassword.view.html',
+			templateUrl : 'app/login/forgotPassword.view.html',
 			controllerAs : 'vm'
 		}).when('/forgotPasswordSuccess', {
 			controller : 'LoginController',
-			templateUrl : 'login/forgotPasswordSuccess.view.html',
+			templateUrl : 'app/login/forgotPasswordSuccess.view.html',
 			controllerAs : 'vm'
 		}).when('/scheduledEventsCalendar', {
 			controller : 'ScheduledEventsCalendarController',
-			templateUrl : 'eventsCalendar/scheduledEventsCalendar.view.html',
+			templateUrl : 'app/eventsCalendar/scheduledEventsCalendar.view.html',
 			controllerAs : 'vm'
 		}).when('/upcomingEventsCalendar', {
 			controller : 'UpcomingEventsCalendarController',
-			templateUrl : 'eventsCalendar/upcomingEventsCalendar.view.html',
+			templateUrl : 'app/eventsCalendar/upcomingEventsCalendar.view.html',
 			controllerAs : 'vm'
 		}).when('/profilePage', {
 			controller : 'ProfilePageController',
-			templateUrl : 'profilePage/profilePage.view.html',
+			templateUrl : 'app/profilePage/profilePage.view.html',
 			controllerAs : 'vm'
 		}).when('/register', {
 			controller : 'RegisterController',
-			templateUrl : 'register/register.view.html',
+			templateUrl : 'app/register/register.view.html',
 			controllerAs : 'vm'
 		}).otherwise({
 			redirectTo : '/login'
@@ -60,7 +60,7 @@
 	function handleLoginFailure($location, $rootScope) {
 		var restrictedPage = $.inArray($location.path(), ['/login', '/forgotPassword', '/forgotPasswordSuccess']) === -1;
 		var loggedIn = $rootScope.isSuccesfullyLoggedin;
-		if ((restrictedPage && !loggedIn) || $location.path() === '/logout') {
+		if (restrictedPage && !loggedIn) {
 			$('.navbar-default').addClass('hide');
 			$location.path('/login');
 		}
--- app/login/logout.controller.js
+++ app/login/logout.controller.js
@@ -17,19 +17,30 @@
          * Logout the user
          */
         function Logout() {
+        	vm.dataLoading = true;
         	var data = {
         		from_prev_call : $rootScope.for_next_call,
         		uID : $rootScope.uID
         	};
         	AuthenticationService.Logout(data).then(function (response) {
+        		vm.dataLoading = false;
         		if (response.data.errorMSG_internal !== 'DEFAULT_OK') {
+        			$('.signout-error-message').html(response.data.errorMSG_user);
 					FlashService.Error(response.data.errorMSG_user);
 				} else {
 					$('.navbar-default').addClass('hide');
 					$location.path('/login');
 				}
-            });
+            }, function error(errorResponse) {
+            	vm.dataLoading = false;
+            	$('.signout-error-message').html(response.data.errorMSG_user);
+				FlashService.Error($rootScope.configData.errorMessage);
+			});
         }
+        
+        $('.signout').click(function(e) {
+			Logout();
+		});
     }
 
 })();
